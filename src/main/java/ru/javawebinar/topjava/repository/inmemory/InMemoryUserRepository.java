60204cb35c043abcb48f2c7c6bf3b305837524d6
u766979787368 <u766979787368@gmail.com>
2021-06-25 23:08:15 +0300

    fix

--- src/main/java/ru/javawebinar/topjava/repository/inmemory/InMemoryUserRepository.java
+++ src/main/java/ru/javawebinar/topjava/repository/inmemory/InMemoryUserRepository.java
@@ -62,6 +62,6 @@ public class InMemoryUserRepository implements UserRepository {
     @Override
     public User getByEmail(String email) {
         log.info("getByEmail {}", email);
-        return repository.values().stream().filter(user -> user.getEmail().equals(email)).findFirst().get();
+        return getAll().stream().filter(user -> user.getEmail().equals(email)).findFirst().orElse(null);
     }
 }
